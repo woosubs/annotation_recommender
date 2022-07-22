8034892d981d12a21dca529165cb28c089b6c3ea
Woosub Shin <woosubs@umich.edu>
2022-07-23 00:21:49 +0900

    updated reaction annotation class

--- annotation_recommender/reaction_annotation.py
+++ annotation_recommender/reaction_annotation.py
@@ -14,7 +14,11 @@ with open(os.path.join(cn.RHEA_DIR, 'rhea_all2bi.pkl'), 'rb') as f:
   ref_rhea2bi = pickle.load(f)
 with open(os.path.join(cn.RHEA_DIR, 'kegg2rhea_bi.pickle'), 'rb') as handle:
   ref_kegg2rhea_bi = pickle.load(handle)
-  with open(os.path.join(cn.ALGO_DIR, 'binary_ref_df.pickle'), 'rb') as handle:
+with open(os.path.join(cn.RHEA_DIR, 'rhea2chebi_reference.pkl'), 'rb') as f:
+  ref_rhea_to_chebi = pickle.load(f)
+with open(os.path.join(cn.CHEBI_DIR, 'chebi_shortened_formula_30apr2022.pickle'), 'rb') as f:
+  ref_shortened_chebi_to_formula = pickle.load(f)
+with open(os.path.join(cn.ALGO_DIR, 'binary_ref_df.pickle'), 'rb') as handle:
     ref_mat = pickle.load(handle)
 
 
@@ -62,7 +66,25 @@ class ReactionAnnotation(object):
     # Attributes after prediction
     self.candidates = None
     self.match_score = None
+    self.sum_match_score = None
     self.query_df = None
+
+  def getMatchScore(self, inp_match_score):
+    """
+    Calculate match scores using 
+    the given dictionary
+  
+    Parameters
+    ----------
+    inp_match_score: dict {reaction_id: {candidate_id: float}}
+  
+    Returns
+    -------
+    match_score: float
+    """
+    return np.sum([np.max([inp_match_score[val][k] \
+                           for k in inp_match_score[val].keys()]) \
+                   for val in inp_match_score.keys()])
 
   def getReactionComponents(self,
                             inp_reaction):
@@ -97,8 +119,9 @@ class ReactionAnnotation(object):
                         inp_ref_mat=ref_mat):
     """
     Predict 1) reaction annotation candidates 
-    and 2) confide`nce of them
+    and 2) match score of them
     using species dict (argument) etc.
+    Also updates several class attributes as a result. 
   
     Parameters
     ----------
@@ -109,14 +132,9 @@ class ReactionAnnotation(object):
       
     Returns
     -------
-    pred_cands: dict
-        Predicted candidates
-        {reaction ID: list of RHEA IDs}
-    pred_conf: dict
+    pred_match_score: dict
         Confidence score of each prediction
         {reaction ID: {Rhea ID: float between 0.0-1.0}}
-    query_df: pandas.DataFrame
-        Query Dataframe for reference 
     """
     # get libsbml.reaction and their IDs
     if inp_reac_list is not None:
@@ -162,11 +180,77 @@ class ReactionAnnotation(object):
       pred_match_score[one_rid] = match_score_per_cand
     self.candidates = pred_cands
     self.match_score = pred_match_score
+    self.sum_match_score = self.getMatchScore(pred_match_score)
     self.query_df = query_df
     return pred_match_score
 
 
-
-
-
-
+  def updateSpeciesByAReaction(self, 
+                               inp_rid, inp_spec_dict,
+                               inp_rhea, inp_ref_mat):
+    """
+    Update predicted species annotation
+    using predicted rhea annotation candidate
+    for a single reaction.
+    Current version works for when there is just
+    one candidate (i.e., one RHEA term).  
+  
+    Parameters
+    ----------
+    inp_rid: str
+        Reactino ID to match the candidate
+    inp_spec_dict: dict
+        {species ID: chemical formula}
+    inp_rhea: str
+        A RHEA term predicted 
+    inp_ref_mat: pandas.DataFrame
+  
+    Returns
+    -------
+    dict: {species ID: [list of CHEBI terms]}
+          Suggested mapping from species ID to a chebi term
+    """
+    # Chebi terms (reference) associated with the given Rhea term
+    # If there is no such Rhea term in the reference, return None
+    if inp_rhea in ref_rhea2bi.keys():
+      if ref_rhea2bi[inp_rhea] in ref_rhea_to_chebi.keys():
+        rhea_term_to_chebi_elements = [val for val in ref_rhea_to_chebi[ref_rhea2bi[inp_rhea]] \
+                                       if val in ref_shortened_chebi_to_formula.keys()]
+      else:
+        return None
+    else:
+      return None
+    # Similarly, species formula (reference) associated with the Rhea term. 
+    one_r_elements_row = inp_ref_mat.loc[inp_rhea, :]
+    one_r_elements = one_r_elements_row[one_r_elements_row!=0].index
+    # Next, species formulas (query) associated with the predicted Rhea term.
+    one_r_query_elements_row = self.query_df.loc[:, inp_rid]
+    one_r_query_elements = one_r_query_elements_row[one_r_query_elements_row!=0].index
+    #
+    # Identify species not included vs. species included (in query), compared to ref_mat
+    # species_not_included = [val for val in one_r_elements if val not in one_r_query_elements]
+    formula_included = [val for val in one_r_elements if val in one_r_query_elements]
+    # {predicted species ID: formula} for all elements in the reaction 
+    all_species_in_a_reaction = self.getReactionComponents(inp_rid)
+    spec2predicted_formula = {one_spec:inp_spec_dict[one_spec] \
+                                       for one_spec in all_species_in_a_reaction}  
+    #
+    possibly_correct_species_ids = []
+    chebi_term_already_used_in_prediction = []
+    for one_incl_formula in formula_included:
+      for one_specid in spec2predicted_formula.keys():
+        # If one included formula can be found in one of the predicted spec_dict,
+        # it will be possibly correct (we are looking for incorrect ones)
+        if one_incl_formula in spec2predicted_formula[one_specid]:
+          possibly_correct_species_ids.append(one_specid)
+          for one_chebi_term in rhea_term_to_chebi_elements:
+            if one_incl_formula == ref_shortened_chebi_to_formula[one_chebi_term]:
+              chebi_term_already_used_in_prediction.append(one_chebi_term)
+    remaining_chebi = [val for val in rhea_term_to_chebi_elements \
+                       if val not in chebi_term_already_used_in_prediction]
+    remaining_specid = [val for val in all_species_in_a_reaction \
+                        if val not in possibly_correct_species_ids]
+    if len(remaining_specid) == 1 and len(remaining_chebi) == 1:    
+      return {remaining_specid[0]: [remaining_chebi[0]]}
+    else:
+      None
