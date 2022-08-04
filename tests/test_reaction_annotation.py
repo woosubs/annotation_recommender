21d583ec8c9dc7b29dc5bcc052909e0daf142742
Woosub Shin <woosubs@umich.edu>
2022-08-04 11:07:17 +0900

    testing reaction annotation class

--- tests/test_reaction_annotation.py
+++ tests/test_reaction_annotation.py
@@ -1,3 +1,37 @@
 # test_reaction_annotation.py
 # Test for ReactionAnnotation class
 
+import libsbml
+import os
+import sys
+import unittest
+
+from annotation_recommender import species_annotation as sa
+from annotation_recommender import reaction_annotation as ra
+from annotation_recommender import constants as cn
+from annotation_recommender import tools
+
+
+E_COLI_PATH = os.path.join(os.getcwd(), 'e_coli_core.xml')
+BIOMD_248_PATH = os.path.join(os.getcwd(), 'BIOMD0000000248.xml')
+# Below ??
+# M_FDP_C = 'M_fdp_c'
+# M_ATP_C = 'M_atp_c'
+# ONESET_SPECIES_IDS = [M_FDP_C, M_ATP_C]
+
+# ID of a reaction
+R_PFK = 'R_PFK'
+
+#############################
+# Tests
+#############################
+class TestReactionAnnotation(unittest.TestCase):
+
+  def setUp(self):
+    self.spec_cl = sa.SpeciesAnnotation(libsbml_fpath = E_COLI_PATH)
+    self.reac_cl = ra.ReactionAnnotation(libsbml_fpath = E_COLI_PATH)
+
+  def testGetMatchScore(self):
+    one_dict = {'R1': {'M1': 0.7}}
+    one_match_score = self.reac_cl.getMatchScore(score_dict=one_dict)
+    self.assertEqual(one_match_score, 0.7)
--- tests/test_species_annotation.py
+++ tests/test_species_annotation.py
@@ -15,6 +15,10 @@ from annotation_recommender import tools
 
 E_COLI_PATH = os.path.join(os.getcwd(), 'e_coli_core.xml')
 BIOMD_248_PATH = os.path.join(os.getcwd(), 'BIOMD0000000248.xml')
+# IDs of species
+M_FDP_C = 'M_fdp_c'
+M_ATP_C = 'M_atp_c'
+ONESET_SPECIES_IDS = [M_FDP_C, M_ATP_C]
 
 
 #############################
@@ -23,4 +27,34 @@ BIOMD_248_PATH = os.path.join(os.getcwd(), 'BIOMD0000000248.xml')
 class TestSpeciesAnnotation(unittest.TestCase):
 
   def setUp(self):
-    self.spec_cl = sa.SpeciesAnnotation(libsbml_path = E_COLI_PATH)
+    self.spec_cl = sa.SpeciesAnnotation(libsbml_fpath = E_COLI_PATH)
+  
+  def testPredictAnnotationByEditDistance(self):
+    one_spec_name = self.spec_cl.model.getSpecies(M_FDP_C).name.lower()
+    one_pred_spec = self.spec_cl.predictAnnotationByEditDistance(inp_str=one_spec_name)
+    self.assertEqual(one_pred_spec[cn.MATCH_SCORE], 1.0)
+    self.assertTrue('CHEBI:16905' in one_pred_spec[cn.CHEBI])
+    self.assertTrue('CHEBI:49299' in one_pred_spec[cn.CHEBI])
+    self.assertEqual(one_pred_spec[cn.FORMULA],  ['C6O12P2'])
+
+  def testPredictAnnotationByName(self):
+    one_pred_spec = self.spec_cl.predictAnnotationByName(inp_spec_list=[M_FDP_C])
+    self.assertEqual(one_pred_spec[M_FDP_C][cn.MATCH_SCORE], 1.0)
+    self.assertTrue('CHEBI:16905' in one_pred_spec[M_FDP_C][cn.CHEBI])
+    self.assertTrue('CHEBI:49299' in one_pred_spec[M_FDP_C][cn.CHEBI])
+    self.assertEqual(one_pred_spec[M_FDP_C][cn.FORMULA],  ['C6O12P2'])
+
+
+  def testGetAccuracy(self):
+    # test with a dummy name
+    dummy_ref1 = {'a': ['ABC', 'BCD'],
+                  'b': ['DEF']}
+    pref_ref1 = {'a': ['ABC'],
+                 'b': ['AAA']}
+    accuracy1 = self.spec_cl.getAccuracy(ref_annotation=dummy_ref1,
+                                            pred_annotation=pref_ref1)
+    self.assertEqual(accuracy1, 0.5)
+    one_pred_spec = self.spec_cl.predictAnnotationByName(inp_spec_list=[M_FDP_C])
+    accuracy2 = self.spec_cl.getAccuracy()
+    self.assertEqual(accuracy2, 1.0)
+    
